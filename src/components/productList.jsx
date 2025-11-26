04c152bc594dedc75b40c5f0f3aa69408ef591c0
newcodesubed <subedshahxi@gmail.com>
2025-11-26 21:45:06 +0545

    fixed product List grid

--- src/components/productList.jsx
+++ src/components/productList.jsx
@@ -1,35 +1,50 @@
 import {  useEffect, useState } from 'react'
 import ProductCard from './productCard';
-
 export default function ProductList() {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState('');
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-    const getData= async ()=>{
-        setLoading(true);
-        try {
-            const response = await fetch('https://dummyjson.com/products');
-            const data = await response.json();
-            setProducts(data.products);
-        } catch (err) {
-            console.log(err);
-            setError('Failed to fetch products');
-        } finally {
-            setLoading(false);
-        }
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+      setProducts(data.products);
+    } catch (err) {
+      console.log(err);
+      setError("Failed to fetch products");
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        getData();
-    }, [])
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div>
-        {loading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-        <div className='grid grid-cols-6 gap-4'>
-            {!loading && !error && products.map(product=>ProductCard({product}))}
-            </div>
+    <div className="px-4 py-6">
+
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
+      <div
+        className="grid 
+        grid-cols-2 
+        sm:grid-cols-3 
+        md:grid-cols-4 
+        lg:grid-cols-5 
+        xl:grid-cols-6 
+        gap-6"
+      >
+        {!loading &&
+          !error &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+
     </div>
-  )
+  );
 }
